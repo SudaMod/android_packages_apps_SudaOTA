ABC-KANG/abc_packages_apps_AbcOTA

ABCOTA: update notifications
* fix notifications on Oreo
* improve strings
* add toast

* Wight554: Thanks the_dise for help with icon

app/src/main/java/com/abc/ota/tasks/CheckUpdateTask.java
@@ -17,6 +17,7 @@
 package com.abc.ota.tasks;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -27,6 +28,7 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.app.NotificationCompat;
 
 import com.abc.ota.MainActivity;
 import com.abc.ota.R;
@@ -103,6 +105,7 @@ public class CheckUpdateTask extends AsyncTask<Context, Void, OTADevice> {
             boolean updateAvailable = OTAVersion.checkServerVersion(latestVersion, mContext);
             if (updateAvailable) {
                 showNotification(mContext);
+                showToast(R.string.update_available);
             } else {
                 showToast(R.string.no_update_available);
             }
@@ -151,22 +154,30 @@ public class CheckUpdateTask extends AsyncTask<Context, Void, OTADevice> {
     }
 
     private void showNotification(Context context) {
-        Notification.Builder builder = new Notification.Builder(context);
-        builder.setContentTitle(context.getString(R.string.notification_title));
-        builder.setContentText(context.getString(R.string.notification_message));
-        builder.setSmallIcon(R.drawable.ic_notification_slimota);
-        builder.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_slimota));
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        int notifyID = 1;
+        String id = "abcota_channel";
+        CharSequence name = context.getString(R.string.abc_channel);
+        String description = context.getString(R.string.abc_channel_description);
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel mChannel = new NotificationChannel(id, name, importance);
+        mChannel.setDescription(description);
+        notificationManager.createNotificationChannel(mChannel);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context.getApplicationContext())
+                .setSmallIcon(R.drawable.ic_notification_abc)
+                .setContentTitle(context.getString(R.string.notification_title))
+                .setContentText(context.getString(R.string.notification_message))
+                .setOnlyAlertOnce(true)
+                .setAutoCancel(true)
+                .setChannelId(id);
 
         Intent intent = new Intent(context, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         final PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
-        builder.setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification notification = builder.build();
-        notification.flags |= Notification.FLAG_AUTO_CANCEL;
-        notificationManager.notify(1000001, notification);
+        mBuilder.setContentIntent(pendingIntent);
+        notificationManager.notify(notifyID, mBuilder.build());
     }
 }

SudaMod/android_packages_apps_SudaOTA

app/src/main/java/com/sudamod/ota/tasks/CheckUpdateTask.java
@@ -17,6 +17,7 @@
 package com.sudamod.ota.tasks;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
@@ -27,6 +28,7 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.app.NotificationCompat;
 
 import com.sudamod.ota.MainActivity;
 import com.sudamod.ota.R;
@@ -103,6 +105,7 @@ public class CheckUpdateTask extends AsyncTask<Context, Void, OTADevice> {
             boolean updateAvailable = OTAVersion.checkServerVersion(latestVersion, mContext);
             if (updateAvailable) {
                 showNotification(mContext);
+                showToast(R.string.update_available);
             } else {
                 showToast(R.string.no_update_available);
             }
@@ -151,22 +154,30 @@ public class CheckUpdateTask extends AsyncTask<Context, Void, OTADevice> {
     }
 
     private void showNotification(Context context) {
-        Notification.Builder builder = new Notification.Builder(context);
-        builder.setContentTitle(context.getString(R.string.notification_title));
-        builder.setContentText(context.getString(R.string.notification_message));
-        builder.setSmallIcon(R.drawable.ic_notification_slimota);
-        builder.setLargeIcon(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_slimota));
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        int notifyID = 1;
+        String id = "abcota_channel";
+        CharSequence name = context.getString(R.string.sm_channel);
+        String description = context.getString(R.string.sm_channel_description);
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel mChannel = new NotificationChannel(id, name, importance);
+        mChannel.setDescription(description);
+        notificationManager.createNotificationChannel(mChannel);
+
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(context.getApplicationContext())
+                .setSmallIcon(R.drawable.ic_notification_abc)
+                .setContentTitle(context.getString(R.string.notification_title))
+                .setContentText(context.getString(R.string.notification_message))
+                .setOnlyAlertOnce(true)
+                .setAutoCancel(true)
+                .setChannelId(id);
 
         Intent intent = new Intent(context, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         final PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent,
                 PendingIntent.FLAG_ONE_SHOT);
-        builder.setContentIntent(pendingIntent);
-
-        NotificationManager notificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        Notification notification = builder.build();
-        notification.flags |= Notification.FLAG_AUTO_CANCEL;
-        notificationManager.notify(1000001, notification);
+        mBuilder.setContentIntent(pendingIntent);
+        notificationManager.notify(notifyID, mBuilder.build());
     }
 }